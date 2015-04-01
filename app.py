b40da768bf6bd5b5b3bf7e78126f3fd8f94f3ba7
Rachel Shorey <rshorey@gmail.com>
2015-03-31 21:13:08 -0400

    added eagle scout and gold award

--- app.py
+++ app.py
@@ -4,20 +4,29 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<state>')
-def index(state=None):
+def get_phrase_count(phrase,state):
     sunlight_api_key = os.environ.get("SUNLIGHT_API_KEY")
     base_url = "http://capitolwords.org/api/1/text.json?apikey={api_key}&start_date=2014-01-01&end_date=2014-12-31".format(api_key=sunlight_api_key)
     if state is not None:
         base_url += "&state={state}".format(state=state.upper())
-    boy_scouts_url = base_url + "&phrase=boy+scouts"
-    girl_scouts_url = base_url + "&phrase=girl+scouts"
-    boy_page = requests.get(boy_scouts_url).json()
-    girl_page = requests.get(girl_scouts_url).json()
-    boy_mentions = boy_page["num_found"]
-    girl_mentions = girl_page["num_found"]
-    return 'Boy Scout mentions: {boy_mentions} Girl Scout mentions: {girl_mentions}'.format(boy_mentions=boy_mentions,girl_mentions=girl_mentions)
+    query_url = base_url + "&phrase={query}".format(query=phrase.replace(" ","+"))
+    query_result = requests.get(query_url).json()
+    return query_result["num_found"]
+
+@app.route('/')
+@app.route('/<state>')
+def index(state=None):
+    boy_mentions = get_phrase_count("boy scouts",state)
+    girl_mentions = get_phrase_count("girl scouts",state)
+    eagle_mentions = get_phrase_count("eagle scout",state)
+    gold_mentions = get_phrase_count("gold award",state)
+    return ('Boy Scouts mentions: {boy_mentions}<br>'\
+        'Girl Scouts mentions: {girl_mentions}<br>'\
+        'Eagle Scout mentions: {eagle_mentions}<br>'\
+        'Gold Award mentions: {gold_mentions}<br>').format(boy_mentions=boy_mentions,
+                                girl_mentions=girl_mentions,
+                                eagle_mentions=eagle_mentions,
+                                gold_mentions=gold_mentions)
 
 if __name__ == '__main__':
     app.run(debug=True)
