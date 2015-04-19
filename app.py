8773d27d63126cdb965c4253e73198b0777400cc
Rachel Shorey <rshorey@gmail.com>
2015-04-19 13:12:12 -0400

    404 page and note if no reps exist

--- app.py
+++ app.py
@@ -71,5 +71,10 @@ def contact(zipcode):
 def about():
     return render_template('about.html')
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
